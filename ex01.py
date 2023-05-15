c02b0eb3001678f950baf4f91eec9efb4d8b0810
Michel <106674127+MichelZero@users.noreply.github.com>
2023-05-15 08:21:13 -0300

    up

---
+++ ex01.py
@@ -0,0 +1,9 @@
+#
+#
+# 1- Faça um programa (em python) que calcule as 
+# raízes da equação do 2 grau conforme a 
+# fórmula de Bhaskara.
+# ax² + bx + c = 0
+# delta = b² - 4ac
+# x1 = (-b + raiz(delta)/2a)
+# x2 = (-b - raiz(delta)/2a)
